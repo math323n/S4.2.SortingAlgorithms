745fd392741de07b29804a81bd5d5b69ab46cd51
math323n <math323n@edu.campusvejle.dk>
2021-01-26 14:46:22 +0100

    added more

--- S4.2.SortingAlgorithms.ConsoleApp/Program.cs
+++ S4.2.SortingAlgorithms.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace S4._2.SortingAlgorithms.ConsoleApp
 {
@@ -59,6 +60,145 @@ namespace S4._2.SortingAlgorithms.ConsoleApp
                     }
                     (array[i], array[k]) = (array[k], array[i]);
                 }
+            }
+        }
+
+        private static void TreeSort(int[] array)
+        {
+            SortedSet<int> t = new();
+            for(int i = 0; i < array.Length; i++)
+            {
+                t.Add(array[i]);
+            }
+            FillArray(t, array, 0);
+        }
+
+        private static int FillArray(SortedSet<int> t, int[] array, int j)
+        {
+            if(t.Count != 0)
+            {
+                j = FillArray(t, array, j);
+                // array[j++] = root(t);
+                //  j = FillArray(t.Max, array, j)
+            }
+            return j;
+        }
+
+        private static void HeapSort(int[] array)
+        {
+            //TODO
+        }
+
+        private static void QuickSort(int[] array, int left, int right)
+        {
+            if(left < right)
+            {
+                int pivotIndex = OtherPartition(array, left, right);
+                QuickSort(array, left, pivotIndex - 1);
+                QuickSort(array, pivotIndex + 1, right);
+            }
+        }
+
+        private static int Partition(int[] array, int left, int right)
+        {
+            int pivotIndex = array[(array.Length - 1) / 2];
+            int pivot = array[pivotIndex];
+            (array[pivotIndex], array[right]) = (array[right], array[pivotIndex]);
+            int leftMark = left;
+            int rightMark = right - 1;
+            while(leftMark <= rightMark)
+            {
+                while(leftMark <= rightMark && array[leftMark] <= pivot)
+                {
+                    leftMark++;
+                }
+                while(leftMark <= rightMark && array[rightMark] >= pivot)
+                {
+                    rightMark--;
+                }
+                if(leftMark < rightMark)
+                {
+                    (array[leftMark++], array[rightMark--]) = (array[rightMark--], array[leftMark++]);
+                }
+            }
+            (array[leftMark], array[rightMark]) = (array[rightMark], array[leftMark]);
+            return leftMark;
+        }
+
+        private static int OtherPartition(int[] array, int left, int right)
+        {
+            int[] newArray = new int[right - left + 1];
+            int pivotIndex = array[(array.Length - 1) / 2];
+            int pivot = array[pivotIndex];
+            int arrayCount = left;
+            int newArrayCount = 1;
+            for(int i = left; i <= right; i++)
+            {
+                if(i == pivotIndex)
+                {
+                    newArray[0] = array[i];
+                }
+                else if(array[i] < pivot || (array[i] == pivot && i < pivotIndex))
+                {
+                    array[arrayCount++] = array[i];
+                }
+                else
+                {
+                    newArray[newArrayCount++] = array[i];
+                }
+            }
+            for(int i = 0; i < newArrayCount; i++)
+            {
+                array[arrayCount++] = newArray[i];
+            }
+            return right - newArrayCount + 1;
+        }
+
+        private static void MergeSort(int[] array, int left, int right)
+        {
+            if(left < right)
+            {
+                int mid = (left + right) / 2;
+                MergeSort(array, left, mid);
+                MergeSort(array, mid + 1, right);
+                Merge(array, left, mid, right);
+            }
+        }
+
+        private static void Merge(int[] array, int left, int mid, int right)
+        {
+            int[] newArray = new int[right - left + 1];
+            int newArrayCount = 0;
+            int lCount = left;
+            int rCount = mid + 1;
+            while((lCount <= mid) && (rCount <= right))
+            {
+                if(array[lCount] <= array[rCount])
+                {
+                    newArray[newArrayCount++] = array[lCount++];
+                }
+                else
+                {
+                    newArray[newArrayCount++] = array[rCount++];
+                }
+            }
+            if(lCount > mid)
+            {
+                while(rCount <= right)
+                {
+                    newArray[newArrayCount++] = array[rCount++];
+                }
+            }
+            else
+            {
+                while(lCount <= mid)
+                {
+                    newArray[newArrayCount++] = array[lCount++];
+                }
+            }
+            for(newArrayCount = 0; newArrayCount < right - left + 1; newArrayCount++)
+            {
+                array[left + newArrayCount] = newArray[newArrayCount];
             }
         }
     }
