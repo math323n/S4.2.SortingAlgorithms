c3d8d54532e4533a3d899348836339f4a91a5628
math323n <math323n@edu.campusvejle.dk>
2021-01-28 14:37:55 +0100

    edit

--- S4.2.SortingAlgorithms.ConsoleApp/Program.cs
+++ S4.2.SortingAlgorithms.ConsoleApp/Program.cs
@@ -19,7 +19,7 @@ namespace S4._2.SortingAlgorithms.ConsoleApp
                 Console.WriteLine(array[i]);
             }
         }
-
+         
         // A function to implement bubble sort.
         // Compares the last item with the one before it, & swaps them if they are not in the correct order. Repeats this process "n" times.
         private static void BubbleSort(int[] array)
@@ -32,27 +32,31 @@ namespace S4._2.SortingAlgorithms.ConsoleApp
                     // if two adjoining elements, are in the wrong order, swap them.
                     if(array[j] < array[j - 1])
                     {
-                        // Code to swap 
+                        // Code to swap, if item number "j - 1" is larger than item "j"
                         (array[j - 1], array[j]) = (array[j], array[j - 1]);
                     }
                     // Repeat above until end of array.
                 }
             }
         }
-
+            
         // Goes through the entire array to check if item "j" is in the correct spot. Repeats for every item.
         private static void InsertionSort(int[] array)
         {
+            // Loop array skipping first element
             for(int i = 1; i < array.Length; i++)
             {
+
                 int j = i;
                 int t = array[j];
+                //While item "j" is NOT ordered correctly, move it around the array until it is
                 while(j > 0 && t < array[j - 1])
                 {
                     array[j] = array[j - 1];
                     j--;
                 }
                 array[j] = t;
+                // Repeat above until end of array.
             }
         }
 
@@ -62,8 +66,10 @@ namespace S4._2.SortingAlgorithms.ConsoleApp
             for(int i = 0; i < array.Length - 1; i++)
             {
                 int k = i;
+                //Find smallest item in array.
                 for(int j = i + 1; j < array.Length; j++)
                 {
+                    //If the current item is smaller than the first unordered item, swap the two items.
                     if(array[j] < array[k])
                     {
                         k = j;
@@ -138,21 +144,28 @@ namespace S4._2.SortingAlgorithms.ConsoleApp
 
         private static int OtherPartition(int[] array, int left, int right)
         {
+            //Create new array half as long as the original array
             int[] newArray = new int[right - left + 1];
+
+            //Find centre index of original array
             int pivotIndex = array[(array.Length - 1) / 2];
             int pivot = array[pivotIndex];
             int arrayCount = left;
             int newArrayCount = 1;
+            // Loop array
             for(int i = left; i <= right; i++)
             {
+                //If item "i" is the centre index, insert "i" into the new array as the first item.
                 if(i == pivotIndex)
                 {
                     newArray[0] = array[i];
                 }
+                //Else if item "i" is smaller than the centre item or is equal to the centre item while preceeding it, move the item to the front //half of the array.
                 else if(array[i] < pivot || (array[i] == pivot && i < pivotIndex))
                 {
                     array[arrayCount++] = array[i];
                 }
+                //Otherwise insert item "i" into the new array.
                 else
                 {
                     newArray[newArrayCount++] = array[i];
@@ -177,7 +190,7 @@ namespace S4._2.SortingAlgorithms.ConsoleApp
         }
 
         private static void Merge(int[] array, int left, int mid, int right)
-        {
+        {   
             int[] newArray = new int[right - left + 1];
             int newArrayCount = 0;
             int lCount = left;
